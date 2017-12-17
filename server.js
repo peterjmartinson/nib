c77c1c504080837f680461234f157b9959148591
Peter <peter.j.martinson@gmail.com>
2017-12-17 17:28:48 -0500

    Insert item into MongoDB database
    The POST route successfully adds an item to the database.  It may need
    to be changed to an *upsert* instead of an insert, since Nib needs to be
    set up for editing existing notes as well as adding new ones.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
   const express    = require('express'),
         app        = express(),
         port       = process.env.PORT || 3000,
+        MongoClient = require('mongodb').MongoClient,
+        assert = require('assert'),
         bodyParser = require('body-parser');
 
   app.use(bodyParser.json());
@@ -17,15 +19,26 @@
 
   app.use(express.static(__dirname + '/public'));
 
-  app.post('/', function(req, res) {
-    console.log("You just tried to post, Sucker!");
-    console.log(req.body.thenote);
-    res.redirect('/');
-  });
+  MongoClient.connect('mongodb://localhost:27017/nib', function(err, db) {
 
-  app.listen(port, function (){
-    console.log('Server is listening to %d port in %s mode',port,app.settings.env);
-  });
+    assert.equal(null, err);
+    console.log("Successfully connected to MongoDB.");
+    app.post('/', function(req, res) {
+      console.log("You just tried to post, Sucker!");
+      console.log(req.body.thenote);
 
+      db.collection("notes").insertOne({
+        created_date : new Date(),
+        note_text : req.body.thenote
+      });
+      res.redirect('/');
+
+    });
+
+    app.listen(port, function (){
+      console.log('Server is listening to %d port in %s mode',port,app.settings.env);
+    });
+
+  });
 
 }())
