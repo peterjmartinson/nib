802e5db66a99fd05c3740a3573aa565ab501a9ad
Peter <peter.j.martinson@gmail.com>
2018-02-24 13:24:57 -0500

    still messing with xhr testing

---
+++ public/dir_test.js
@@ -0,0 +1,1 @@
+console.log("in public directory");
--- public/test/nib.test.js
+++ public/test/nib.test.js
@@ -1,24 +1,26 @@
 // const assert = window.assert;
 const assert = chai.assert;
 
-describe('The canary', function() {
-  it('should tweet', function() {
+describe("The canary", function() {
+  it("should tweet", function() {
     assert.equal(1, 1);
   });
 });
 
-describe('$get', function() {
-  it('should exist', function() {
-    assert.equal(typeof window.handler.$get, 'function');
+describe("$get", function() {
+  it("should exist", function() {
+    assert.equal(typeof window.handler.$get, "function");
   });
 
-  it('should make a new XMLHttpRequest', function() {
-    let original_xhr = XMLHttpRequest;
-    let ran = false;
-    let XMLHttpRequest = function() { ran = true; };
+  it("should make a new XMLHttpRequest", function(done) {
+    var original_xhr = XMLHttpRequest;
+    var ran = false;
+    var XMLHttpRequest = function() { ran = true; };
+    console.log(XMLHttpRequest);
 
-    window.handler.$get('', console.log);
+    window.handler.$get("", console.log);
     assert.equal(ran, true);
-  )};
+    done();
+  });
 
 });
