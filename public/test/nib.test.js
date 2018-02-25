1e915577e9263d21e42a4816bced6314bb5acbca
Peter <peter.j.martinson@gmail.com>
2018-02-24 21:51:11 -0500

    Get XMLHttpRequest almost set up right

--- public/test/nib.test.js
+++ public/test/nib.test.js
@@ -12,15 +12,41 @@ describe("$get", function() {
     assert.equal(typeof window.handler.$get, "function");
   });
 
-  it("should make a new XMLHttpRequest", function(done) {
-    var original_xhr = XMLHttpRequest;
-    var ran = false;
-    var XMLHttpRequest = function() { ran = true; };
+  before (function() {
+    let original_xhr = new XMLHttpRequest(); // not a constructor?
+    console.log(original_xhr);
+    let ran = false;
+    XMLHttpRequest = function() {
+      ran = true;
+      return {
+        verb: '',
+        route: '',
+        open: function(verb, route) {
+          this.verb = verb;
+          this.route = route;
+        },
+        send: function() { return false; },
+        readyState: 4,
+        status: 200,
+        responseText: "Test response text",
+        onreadystatechange: null
+      };
+    };
     console.log(XMLHttpRequest);
+  });
 
-    window.handler.$get("", console.log);
-    assert.equal(ran, true);
-    done();
+  it("should make a new XMLHttpRequest", function(done) {
+
+    window.handler.$get("route", function(response) {
+      console.log(response);
+      assert.equal(1,2);
+      assert.equal(response, "Tests response text");
+      done();
+    });
+    // console.log(XMLHttpRequest.verb);
+    // assert.equal(ran, true);
+    // assert.equal(XMLHttpRequest.verb, "GET");
+    // done();
   });
 
 });
