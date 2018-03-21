d5af0a91dca4da26f724cb298e6350fb2b019156
Peter <peter.j.martinson@gmail.com>
2018-03-20 20:44:31 -0400

    comment out the bad code

--- public/js/nib.js
+++ public/js/nib.js
@@ -26,16 +26,30 @@ let Handler = function() {
     }
   }
 
-  var save_note_button = document.getElementById("save-note");
-  save_note_button.addEventListener("click", postNote, false);
+  // var save_note_button = document.getElementById("save-note");
+  // save_note_button.addEventListener("click", postNote, false);
 
-  function postNote() {
-    let note_text = document.getElementById("note").value;
-    let created_date = new Date();
-    let modified_date = created_date;
-    let note_id = Date.parse(created_date);
-    // check thiS!!!
-  }
+  /*
+   * 1. call the save_note handler function (A)
+   * 2. handler gets the text of the note
+   * 3. Somehow, if it's an exisiting note, the note_id has to be gotten too
+   * 4. handler passes the note text to the posting function (B)
+   * 5. Function B --- this is shit.  You need to *initiate a note editing session*, which will create the note_id automatically
+   *  The note editing session is initiated by either 1. hitting an "initiate" button, or 2. clicking on one of the existing notes.
+  */
+  // function postNote(note_text, note_id, created_date) {
+  //   let post_note_text = document.getElementById("note").value;
+  //   let post_created_date = new Date();
+  //   let post_modified_date = created_date;
+  //   let post_note_id = Date.parse(created_date);
+  //   return {
+  //     note_text: note_text,
+  //     created_date: created_date,
+  //     modified_date: modified_date,
+  //     note_id: note_id
+  //   }
+  //   // check thiS!!!
+  // }
 
   function $get(route, callback) {
     let DONE = 4, OK = 200,
@@ -102,7 +116,7 @@ let Handler = function() {
 
   return {
     getNote: getNote,
-    postNote: postNote,
+    // postNote: postNote,
     $get: $get,
     $post: $post
   };
