4d30fe11023d432f064ab14f7514a692fe8f144e
Peter <peter.j.martinson@gmail.com>
2018-06-01 08:32:41 -0400

    Make failing test

--- public/test/controller.test.js
+++ public/test/controller.test.js
@@ -8,6 +8,13 @@ describe('Controller', function() {
     getTestData: function() {
       return this.test_data;
     },
+    test_error : '',
+    setTestError: function(new_error) {
+      this.test_error = new_error;
+    },
+    getTestError: function() {
+      return this.test_error;
+    }
   };
 
   let view = {};
@@ -21,7 +28,8 @@ describe('Controller', function() {
     model.getOneNote = function(arg, callback)
     {
       let data = this.getTestData();
-      callback(null, data);
+      let error = this.getTestError();
+      callback(error, data);
     }
 
     // =================================================  Tests
@@ -38,9 +46,49 @@ describe('Controller', function() {
 
     it('should call model.getOneNote()', function(done) {
       let test_data = 'called'
+      let test_error = null;
       model.setTestData(test_data);
+      model.setTestError(test_error);
       ctrl.editExistingNote("1000", function(error, data) {
         assert.equal(data, test_data);
+        done();
+      });
+    });
+
+    it('should pass any errors right through to model.getOneNote()', function(done) {
+      let test_error = '404';
+      let test_data = null;
+      model.setTestData(test_data);
+      model.setTestError(test_error);
+      ctrl.editExistingNote('1000', function(error, data) {
+        assert.equal(error, test_error);
+        done();
+      });
+    });
+
+    it('should only pass one object to model, not an array', function(done) {
+      let test_error = null;
+      let test_data = [
+        {
+          "_id": "01",
+          "note_text": "Text of note 01",
+          "created_date": "2017-12-01T19:34:14.565Z",
+          "modified_date": "",
+          "title": "Title 01"
+        },
+        {
+          "_id": "01",
+          "note_text": "Alternate Text of note 01",
+          "created_date": "2017-12-01T19:34:14.565Z",
+          "modified_date": "",
+          "title": "Title 01"
+        }
+      ];
+      model.setTestData(test_data);
+      model.setTestError(test_error);
+      ctrl.editExistingNote('01', function(error, data) {
+        console.log(data);
+        assert.deepEqual(data, test_data[0]);
         done();
       });
     });
